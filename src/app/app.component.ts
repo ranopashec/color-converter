1c2e3650ccd58c3c0442f3e723710f887280477f
ranopashec <semenpetrakov@gmail.com>
2024-09-07 20:38:00 +0300

    conversion without validation and not checked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,66 @@
-import { Component } from '@angular/core';
-import { ColorPickerModule } from 'ngx-color-picker';
-
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms"; // Import FormsModule
+import { ColorPickerModule } from "ngx-color-picker";
+import { NgClass } from "@angular/common";
+import { RGB, CMYK, XYZ, HLS } from "./models/schemes";
+import {
+  convertCMYKtoHLS,
+  convertHLStoRGB,
+  convertRGBtoHLS,
+  convertRGBtoXYZ,
+  convertXYZtoRGB,
+  convertHLStoCMYK,
+} from "./services/converters";
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [ColorPickerModule ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [ColorPickerModule, FormsModule, NgClass],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
-
 export class AppComponent {
-  title = 'color-converter';
-  color = "red"
+  title = "color-converter";
+
+  rgb: RGB = { r: null, g: null, b: null };
+
+  cmyk: CMYK = { c: null, m: null, y: null, k: null };
+  xyz: XYZ = { x: null, y: null, z: null };
+  hls: HLS = { h: null, l: null, s: null };
+
+  reset() {
+    this.rgb = { r: null, g: null, b: null };
+    this.cmyk = { c: null, m: null, y: null, k: null };
+    this.xyz = { x: null, y: null, z: null };
+    this.hls = { h: null, l: null, s: null };
+  }
+  onCMYKChange(event: any) {
+    for (const key in this.cmyk) {
+      const value = this.cmyk[key as keyof CMYK];
+      if (value && (value > 100 || value < 0)) {
+        this.cmyk[key as keyof CMYK] = null;
+      }
+    }
+    if (this.cmyk.c && this.cmyk.m && this.cmyk.y && this.cmyk.k) {
+      this.hls = convertCMYKtoHLS(this.cmyk);
+      this.xyz = convertRGBtoXYZ(convertHLStoRGB(this.hls));
+    }
+  }
+
+  onHLSChange(event: any) {
+    // TODO: VALIDATION
+    if (this.hls.h && this.hls.l && this.hls.s) {
+      this.cmyk = convertHLStoCMYK(this.hls);
+      this.xyz = convertRGBtoXYZ(convertHLStoRGB(this.hls));
+    }
+  }
+  onXYZChange(event: any) {
+    // TODO: VALIDATION
+    if (this.xyz.x && this.xyz.y && this.xyz.z) {
+      this.hls = convertRGBtoHLS(convertXYZtoRGB(this.xyz));
+      this.cmyk = convertHLStoCMYK(this.hls);
+    }
+  }
+  hoverCMYK = false;
+  hoverHLS = false;
+  hoverXYZ = false;
 }
---
+++ src/app/models/schemes.ts
@@ -0,0 +1,22 @@
+export interface RGB {
+  r: number | null;
+  g: number | null;
+  b: number | null;
+}
+export interface HLS {
+  h: number | null;
+  l: number | null;
+  s: number | null;
+}
+export interface CMYK {
+  c: number | null;
+  m: number | null;
+  y: number | null;
+  k: number | null;
+}
+
+export interface XYZ {
+  x: number | null;
+  y: number | null;
+  z: number | null;
+}
